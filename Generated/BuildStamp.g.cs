7f6f12a5fc41359c3cc8ae1e5ebe8cef09bacac1
Aesteralias <Aesteralias@proton.me>
2026-05-15 19:53:11 +0100

    Version number

--- Generated/BuildStamp.g.cs
+++ Generated/BuildStamp.g.cs
@@ -1,6 +1,6 @@
 namespace DynamicShock;
 public static class BuildStamp
 {
-public const string Stamp = "20260515185138";
-public const string Version = "1.0.1+20260515185138";
+public const string Stamp = "20260515185307";
+public const string Version = "1.1.0+20260515185307";
 }
